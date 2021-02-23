25f787859d45e1f7ee4ef4d14b85dc2ea82a86dc
Roeland <roelandsteevan@gmail.com>
2021-02-23 19:44:36 +0100

    reddit and wiki icons added

--- api_site/helpers/renderDetailUI.js
+++ api_site/helpers/renderDetailUI.js
@@ -1,16 +1,12 @@
+const body = document.querySelector('body');
+
 export function renderDetailUI(dataset) {
-    //datasets
     const launch = dataset.launchInfo;
     const rocket = dataset.rocketInfo;
-    const body = document.querySelector('body');
     const launchNameContent = 
         launch.launch_name.includes(' (v1.0)') ? 
         launch.launch_name.replace(' (v1.0)', '') : 
         launch.launch_name;
-    const redditHref = 
-        launch.launch_links.reddit.campaign ?
-        launch.launch_links.reddit.campaign :
-        launch.launch_links.reddit.recovery;
     const dateParts = launch.launch_date_utc.split('T');
     
     // making header elements
@@ -19,8 +15,6 @@ export function renderDetailUI(dataset) {
     const launchDate = document.createElement('p');
     const launchTime = document.createElement('p');
     const imgContainer = document.createElement('div');
-    const redditLink = document.createElement('a');
-    const redditImg = document.createElement('img');
     const backgroundOverlay = document.createElement('div');
     const backgroundContainer = document.createElement('div');
 
@@ -28,16 +22,13 @@ export function renderDetailUI(dataset) {
     backgroundContainer.style.backgroundImage = "url(\"" + rocket.images[0] + "\")";
     backgroundOverlay.classList.add('background-overlay');
     imgContainer.classList.add('img-container');
-    redditLink.setAttribute('href', redditHref);
-    redditLink.setAttribute('target', '_blank');
-    redditImg.setAttribute('src', 'img/reddit-icon.svg');
 
     // assign content to elements
     detailTitle.innerHTML = launchNameContent;
     launchDate.innerHTML = dateParts[0];
     launchTime.innerHTML = dateParts[1].substring(0,5) + '  UTC' ;
 
-    // header elements
+    // append elements
     body.appendChild(header);
     header.appendChild(detailTitle);
     header.appendChild(launchDate);
@@ -51,8 +42,22 @@ export function renderDetailUI(dataset) {
     };
 
     header.appendChild(imgContainer);
-    imgContainer.appendChild(redditLink);
-    redditLink.appendChild(redditImg)
+    header.appendChild(backgroundOverlay);
+    backgroundOverlay.appendChild(backgroundContainer);
+    
+    if (launch.launch_links.reddit.recovery) {
+        const redditHref = 
+        launch.launch_links.reddit.campaign ?
+        launch.launch_links.reddit.campaign :
+        launch.launch_links.reddit.recovery;
+        const redditLink = document.createElement('a');
+        const redditImg = document.createElement('img');
+        redditLink.setAttribute('href', redditHref);
+        redditLink.setAttribute('target', '_blank');
+        redditImg.setAttribute('src', 'img/reddit-icon.svg');
+        imgContainer.appendChild(redditLink);
+        redditLink.appendChild(redditImg);
+    };
 
     if (launch.launch_links.wikipedia) {
         const wikiLink = document.createElement('a');
@@ -64,17 +69,21 @@ export function renderDetailUI(dataset) {
         wikiLink.appendChild(wikiImg)
     };
 
-    header.appendChild(backgroundOverlay);
-    backgroundOverlay.appendChild(backgroundContainer);
-
-    renderLaunchSection(dataset);
+    renderPayloadSection(dataset);
 };
 
-function renderLaunchSection(dataset) {
+function renderPayloadSection(dataset) {
+    const launch = dataset.launchInfo;
+    const rocket = dataset.rocketInfo;
     const launchpad = dataset.launchpadInfo;
     const payload = dataset.payloadInfo;
 
     // making payload elements
     const payloadSection = document.createElement('section');
+
+    // adding attributes
     payloadSection.classList.add('detail-section');
+
+    // append elements
+    body.appendChild(payloadSection);
 };
