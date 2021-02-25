cc9c1309e1d2a5f0dd5aa92b86915cc552191faa
Roeland <roelandsteevan@gmail.com>
2021-02-25 18:38:15 +0100

    top position done with js

--- api_site/helpers/renderDetailUI.js
+++ api_site/helpers/renderDetailUI.js
@@ -148,6 +148,8 @@ export function renderRocketSection(dataset) {
     //if header is taller then screen the main must position lower
     if (header.offsetHeight + 250 > screen.height) {
         main.style.top = header.offsetHeight + 150 + 'px';
+    } else if (window.innerWidth < 1100) {
+        main.style.top = window.innerHeight - 70 + 'px';
     };
 
     // adding content
